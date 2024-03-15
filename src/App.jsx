f1b1968e55c5dad809095e14a3dbc33a17684335
Prajwal S <prajwalplays@gmail.com>
2024-03-15 23:51:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Navbar from './components/Navbar'
-import Timer from './components/Timer'
+import Navbar from "./components/Navbar";
+import Timer from "./components/Timer";
+import { SettingsProvider } from "./context/SettingsContext";
 
 function App() {
-
   return (
-    <div>
-      <Navbar />
-      <Timer />
-    </div>
-      
-  )
+    <SettingsProvider>
+      <div>
+        <Navbar />
+        <Timer />
+      </div>
+    </SettingsProvider>
+  );
 }
 
-export default App
+export default App;
