8a7b0fa2a1721bfafed30c64544b0ff0c63d50ee
Prajwal S <prajwalplays@gmail.com>
2024-03-16 20:12:58 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='text-text font-inconsolata tracking-tighter bg-componentsbg text-center absolute bottom-5 w-full text-sm leading-10'>
+      Made by <a href="https://github.com/notprjwl" className='hover:text-[#e1e1e1]'>prjwl</a>
+    </footer>
+  );
+};
+
+export default Footer;
