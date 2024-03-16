d593649b93e85ab3a490673e7e39acc19850c9db
Prajwal S <prajwalplays@gmail.com>
2024-03-16 20:13:08 +0530

    Update SettingsContext.jsx

--- src/context/SettingsContext.jsx
+++ src/context/SettingsContext.jsx
@@ -7,8 +7,9 @@ export const SettingsProvider = ({ children }) => {
   const [shortBreak, setShortBreak] = useState('5');
   const [longBreak, setLongBreak] = useState('15');
   const [selectedOption, setSelectedOption] = useState("Pomodoro");
+  const [time, setTime] = useState(0)
 
-  return <SettingsContext.Provider value={{ pomodoro, setPomodoro, shortBreak, setShortBreak, longBreak, setLongBreak, selectedOption, setSelectedOption }}>{children}</SettingsContext.Provider>;
+  return <SettingsContext.Provider value={{ pomodoro, setPomodoro, shortBreak, setShortBreak, longBreak, setLongBreak, selectedOption, setSelectedOption, time, setTime }}>{children}</SettingsContext.Provider>;
 };
 
 export default SettingsContext;
