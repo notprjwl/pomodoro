141dc217e15a1f266ae8803d70a779bf23cfc2ac
Prajwal S <prajwalplays@gmail.com>
2024-03-16 20:13:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className='container text-text bg-background font-inconsolata max-w-[600px] mx-auto my-0 flex items-center p-3 pt-2 justify-between'>
       <div>
-        <h1 className='text-2xl'>Pomodoro</h1>
+        <h1 className='text-2xl hover:text-[#e1e1e1] cursor-pointer'>Pomodoro</h1>
       </div>
       <div className='outline outline-1 outline-outline p-1 px-3 rounded-lg hover:text-[#e1e1e1] hover:bg-hoverBg active:bg-blue-900'>
         <button className='text-[1rem]' onClick={handleSettingsClick}>Settings</button>
